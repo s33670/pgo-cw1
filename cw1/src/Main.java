499ac47a92c20f84486c78bb2c7b8cfe37aa2cea
s33670 <s33670@pjwstk.edu.pl>
2025-03-06 09:38:15 +0100

    dodane int

--- cw1/src/Main.java
+++ cw1/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("fdnbdfl");
-
+        int a = 5;
+        int b = 7;
+        System.out.println(a + b);
     }
 }
